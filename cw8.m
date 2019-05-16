7d17d7f38be37141395cf6f98d99508c243d4fb6
Nightkeyman <nightkeyman@gmail.com>
2019-05-16 12:09:07 +0200

    Cwicz 8 update

--- cw8.m
+++ cw8.m
@@ -3,34 +3,45 @@ format long
 format compact 
 
 % cwiczenie 8 
-% Dystrybuanta osi¹ga wartoœci od 0 do 1. W dodatku w minus niesk. jest
-% zero, a w plus niesk. jest 1. Wiêc sprawdzamy czy te funkcje mog¹
-% przekroczyæ te wartoœci. 
+% Dystrybuanta osiÄ…ga wartoÅ›ci od 0 do 1. W dodatku w minus niesk. jest
+% zero, a w plus niesk. jest 1. WiÄ™c sprawdzamy czy te funkcje mogÄ…
+% przekroczyÄ‡ te wartoÅ›ci.
 
-% Jakiœ du¿y zakres argumentów 
+% JakiÅ› duÅ¼y zakres argumentÃ³w 
 X = -1e6:1:1e6;
 
-% 1 
+% 1 - dobry
 Y1 = 0.5 + (1/pi)*atan(X);
 figure(1)
 subplot(2,2,1)
 plot(X,Y1);
 % czyli ten jest spoko, bo od 0 do 1 b
 
-% 2 - ten chyba z³y 
+% 2 - dobry
 Y2 = zeros(size(X));
 for i=1:1:length(X)
- Y2(i) = (1-exp(-X(i))).*heaviside(X(i));
+    if(X(i) < 0)
+     Y2(i) = 0;
+    else
+     Y2(i) = (1-exp(-X(i)));
+    end
 end
 subplot(2,2,2)
 plot(X,Y2);
 
-% 3 - ten na bank z³y
+% 3 - ten na bank zÅ‚y
 Y3 = exp((-X).^2);
 subplot(2,2,3);
 plot(X,Y3);
 
-% 4 - te¿ nie mo¿e :(
-Y4 = (X.^2).*heaviside(X);
+% 4 - teÅ¼ nie moÅ¼e :(
+Y4 = zeros(size(X));
+for j=1:length(X),
+    if(X(j) < 0)
+     Y4(j) = 0;
+    else
+     Y4(j) = X(j)^2;
+    end
+end
 subplot(2,2,4);
 plot(X,Y4);
